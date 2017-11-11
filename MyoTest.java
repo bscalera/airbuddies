b2e5357cb3d747560abf6518b671a1052f7f0554
rhMyCode <rh328@njit.edu>
2017-11-11 18:59:27 -0500

    Create MyoTest.java

---
+++ MyoTest.java
@@ -0,0 +1,32 @@
+package MyoTest;
+
+import java.util.Scanner;
+
+public class MyoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);	
+	 
+		String v ="a";
+		while (!v.equals("q")) {
+			System.out.println("Please enter a value: ");
+			v = scanner.nextLine();
+			System.out.println("Value typed:  " + v);
+		
+		}
+		
+/*		
+ * 
+ * 
+		
+		
+		Threshold[][] pod = new Threshold[4][8];
+		
+		pod[0][0].max=0;
+		pod[0][0].min=0;
+	*/
+		
+	}
+
+}
