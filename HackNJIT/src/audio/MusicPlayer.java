8125ab1687504397de27a73fd9de40ab501f566d
djtrikhster <djtrikhster@gmail.com>
2017-11-12 00:40:26 -0500

    Program Now plays a sound on start up

--- HackNJIT/src/audio/MusicPlayer.java
+++ HackNJIT/src/audio/MusicPlayer.java
@@ -8,7 +8,15 @@ import java.util.Scanner;
 
 public class MusicPlayer {
 	public static void main(String[] args) {
+		
+		try {
+			AudioClip clip = Applet.newAudioClip(new URL("file:///C:/Users/tesht/workspace//HackNJIT/src/audio/CB_Kick.wav"));
+			clip.play();
+		} catch (MalformedURLException murle) {
+			System.out.println(murle);
+		}
 		System.out.println("start");
+		player("CB_Clap");
 		Guesture();
 		System.out.println("stop");
 	}
@@ -22,24 +30,24 @@ public class MusicPlayer {
 
 			System.out.println(c);
 			if (c == 1) {
-				play("CB_Drum");
+				player("CB_Drum");
 			}
 			if (c == 2) {
-				play("CB_Drum");
+				player("CB_Drum");
 			}
 			if (c == 3) {
-				play("CB_Drum");
+				player("CB_Drum");
 			}
 			if (c == 4) {
-				play("CB_Drum");
+				player("CB_Drum");
 			}
 		} while (c != 6);
 		sc.close();
 	}
 
-	public static void play(String instrument) {
+	public static void player(String instrument) {
 		try {
-			AudioClip clip = Applet.newAudioClip(new URL("file:///C:/Users/tesht/workspace//HackNJIT/src/audio/" + instrument + ".wav"));
+			AudioClip clip = Applet.newAudioClip(new URL("file:///C:/Users/tesht/Documents/GitHub/airbuddies/HackNJIT/" + instrument + ".wav"));
 			clip.play();
 		} catch (MalformedURLException murle) {
 			System.out.println(murle);
